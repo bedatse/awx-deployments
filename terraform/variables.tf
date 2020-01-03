b27cf12916a8ddcb191d8d7664244dbb753b6c75
Beda Tse <beda@coeus.hk>
2020-01-03 11:16:41 +0800

    fix variables

---
+++ terraform/variables.tf
@@ -0,0 +1,7 @@
+variable "azurerm_client_id" {
+  description = "The Client ID (appId) for the Service Principal used for the AKS deployment"
+}
+
+variable "azurerm_client_secret" {
+  description = "The Client Secret (password) for the Service Principal used for the AKS deployment"
+}
