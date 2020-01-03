8424173177d249faf888edc33f12beb204f239f0
Beda Tse <beda@coeus.hk>
2020-01-03 14:19:12 +0800

    test helm deployment on aks

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,3 +14,19 @@ module "aks" {
 
   public_ssh_key       = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDRFkSMRhSPT3YftfomhPVrEHHC8OWZ/nWPTNlnIDIK+kApe08dXHDUqJpwX7q6FAZTTD0//ctSX2xIHvbFyEhOOaRvsVBn61cxwR3kD470QyQzGDtvmVqWtTYDcYzQHP+t8jDHWdgX/5OFCQVcTwLUc4+ErM6kFZAELvzpAqnLuly8DQJ80KDhcOBmseJP+zIqUA6/kSDpOSruOyNKTLg8BC1Nn/6cT7GdltBGem2ZNfSThHGCloSjrJOckYFN7rGdhfbmV0kjWJzj0TMSKXH81f18YCcvQxpihBS2k+LsdpTMyVM7rPsZoWGmxsAohmpNMAnDx2HZue074eQr0ZtxUrFZu58SFG8ljuHYX68wBPMh6SUqDco2uZDBCj6coIZHjcf37njMqnyEBV0U8MdZK/v6N0tsU8+w26c4JRS30INyOvo8yVxGg6XWqfAJVMxx4ZBdTCrUgzUDAkgQUSPYZEFV59w3WC4zAtTdN5mNJzxwTtt9FnsSEfrbon6c89/RPWW9VT0QsByvtqSRG1vaAqthgtxoG2xNKOJcU3qU+Uu3MAhBm+5eXAAAXIYraRUmITgJTkpxYNpRzcCurWLGPMGe7mk2KJJcldPTL9tKAVhpcFJy9hXbM2qDYhCsT/WyQwbxmFf4Z0lGYDCswCpqq0w6hlKmZix7036+JuLl9Q=="
 }
+
+provider "helm" {
+  kubernetes {
+    host     = module.aks.host
+
+    client_certificate     = base64decode(module.aks.client_certificate)
+    client_key             = base64decode(module.aks.client_key)
+    cluster_ca_certificate = base64decode(module.aks.cluster_ca_certificate)
+  }
+}
+
+resource "helm_release" "awx" {
+  name    = "awx"
+  chart   = "adwerx/awx"
+  version = "2.0.3"
+}
