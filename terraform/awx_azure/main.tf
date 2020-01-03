d0ef7548e3ccb84929d69002b4a10456c815a466
Beda Tse <beda@coeus.hk>
2020-01-03 17:49:08 +0800

    remove rbac

--- terraform/awx_azure/main.tf
+++ terraform/awx_azure/main.tf
@@ -147,36 +147,36 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.awx.kube_config.0.cluster_ca_certificate)
 }
 
-resource "kubernetes_service_account" "tiller_sa" {
-  metadata {
-    name      = "tiller"
-    namespace = "kube-system"
-  }
-}
+# resource "kubernetes_service_account" "tiller_sa" {
+#   metadata {
+#     name      = "tiller"
+#     namespace = "kube-system"
+#   }
+# }
 
-resource "kubernetes_cluster_role_binding" "tiller_sa_cluster_admin_rb" {
-  metadata {
-    name = "tiller-cluster-role"
-  }
-  role_ref {
-    kind      = "ClusterRole"
-    name      = "cluster-admin"
-    api_group = "rbac.authorization.k8s.io"
-  }
-  subject {
-    kind      = "ServiceAccount"
-    name      = "${kubernetes_service_account.tiller_sa.metadata.0.name}"
-    namespace = "kube-system"
-    api_group = ""
-  }
-}
+# resource "kubernetes_cluster_role_binding" "tiller_sa_cluster_admin_rb" {
+#   metadata {
+#     name = "tiller-cluster-role"
+#   }
+#   role_ref {
+#     kind      = "ClusterRole"
+#     name      = "cluster-admin"
+#     api_group = "rbac.authorization.k8s.io"
+#   }
+#   subject {
+#     kind      = "ServiceAccount"
+#     name      = kubernetes_service_account.tiller_sa.metadata.0.name
+#     namespace = "kube-system"
+#     api_group = ""
+#   }
+# }
 
 provider "helm" {
   debug           = true
-  namespace       = "kube-system"
-  service_account = "tiller"
-  install_tiller  = "true"
-  tiller_image    = "gcr.io/kubernetes-helm/tiller:v${var.TILLER_VER}"
+  # namespace       = "kube-system"
+  # service_account = "tiller"
+  # install_tiller  = "true"
+  # tiller_image    = "gcr.io/kubernetes-helm/tiller:v${var.TILLER_VER}"
 
   kubernetes {
     host                   = azurerm_kubernetes_cluster.awx.kube_config.0.host
